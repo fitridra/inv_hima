758032345aaf01628a3baaff61d70f0d844282df
Fitri Dwi Rahmatulaini <72156771+fitridra@users.noreply.github.com>
2022-02-07 00:20:19 +0700

    Update pilihanlogin.java

--- src/tubes_sda/pilihanlogin.java
+++ src/tubes_sda/pilihanlogin.java
@@ -69,9 +69,6 @@ public class pilihanlogin extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
